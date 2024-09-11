71fe64c723b24cd0a0dd4fb4fdfcc53c4392c38a
Tanmai <chinnitanmai944@kgpian.iitkgp.ac.in>
2024-09-12 00:20:54 +0530

    Update Billing System.cpp
    MAC VERSION

--- Billing System.cpp
+++ Billing System.cpp
@@ -1,195 +1,185 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <windows.h>
+#include <unistd.h> // Use this for usleep on macOS and Linux
 using namespace std;
 
-class Bill{
+class Bill {
 private:
- string Item;
- int Rate, Quantity;
+    string Item;
+    int Rate, Quantity;
 public:
- Bill():Item(""), Rate(0), Quantity(0){ }
- 
- void setItem(string item){
- Item = item;
- }
- 
-void setRate(int rate){
- Rate = rate;
-}
+    Bill() : Item(""), Rate(0), Quantity(0) { }
 
-void setQuant(int quant){
- Quantity = quant;
-}
+    void setItem(string item) {
+        Item = item;
+    }
 
-string getItem(){
-return Item;
-}
+    void setRate(int rate) {
+        Rate = rate;
+    }
 
-int getRate(){
- return Rate;
-}
+    void setQuant(int quant) {
+        Quantity = quant;
+    }
 
-int getQuant(){
- return Quantity;
-}
+    string getItem() {
+        return Item;
+    }
 
+    int getRate() {
+        return Rate;
+    }
+
+    int getQuant() {
+        return Quantity;
+    }
 };
 
-addItem(Bill b){
-bool close = false;
-while(!close){
-	int choice;
-cout<<"\t1.Add."<<endl;
-cout<<"\t2.close."<<endl;
-cout<<"\tEnter Choice: ";
-cin>>choice;
+void addItem(Bill b) {
+    bool close = false;
+    while (!close) {
+        int choice;
+        cout << "\t1.Add." << endl;
+        cout << "\t2.Close." << endl;
+        cout << "\tEnter Choice: ";
+        cin >> choice;
 
-if(choice==1){
-	system("cls");
-string item;
-int rate, quant;
+        if (choice == 1) {
+            system("clear"); // Use "clear" for macOS/Linux instead of "cls"
+            string item;
+            int rate, quant;
 
-cout<<"\tEnter Item Name: ";
-cin>>item;
-b.setItem(item);
+            cout << "\tEnter Item Name: ";
+            cin >> item;
+            b.setItem(item);
 
-cout<<"\tEnter Rate Of Item: ";
-cin>>rate;
-b.setRate(rate);
+            cout << "\tEnter Rate Of Item: ";
+            cin >> rate;
+            b.setRate(rate);
 
-cout<<"\tEnter Quantity Of Item: ";
-cin>>quant;
-b.setQuant(quant);
+            cout << "\tEnter Quantity Of Item: ";
+            cin >> quant;
+            b.setQuant(quant);
 
-ofstream out("D:/Bill.txt", ios::app);
-if(!out){
-	cout<<"\tError: File Can't Open!"<<endl;
-}
-else{
-out<<"\t"<<b.getItem()<<" : "<<b.getRate()<<" : "<<b.getQuant()<<endl<<endl;
-}
-out.close();
-cout<<"\tItem Added Successfuly"<<endl;
-Sleep(3000);
-}
-
-else if(choice == 2){
-	system("cls");
-close = true;
-cout<<"\tBack To Main Menu!"<<endl;
-Sleep(3000);
-}
-}
+            ofstream out("Bill.txt", ios::app);
+            if (!out) {
+                cout << "\tError: File Can't Open!" << endl;
+            } else {
+                out << "\t" << b.getItem() << " : " << b.getRate() << " : " << b.getQuant() << endl << endl;
+            }
+            out.close();
+            cout << "\tItem Added Successfully" << endl;
+            usleep(3000000); // Sleep for 3 seconds
+        } else if (choice == 2) {
+            system("clear");
+            close = true;
+            cout << "\tBack To Main Menu!" << endl;
+            usleep(3000000); // Sleep for 3 seconds
+        }
+    }
 }
 
-printBill(){
-	system("cls");
-int count = 0;
-bool close = false;
-while(!close){
-system("cls");
-int choice;
-cout<<"\t1.Add Bill."<<endl;
-cout<<"\t2.Close Session."<<endl;
-cout<<"\tEnter Choice: ";
-cin>>choice;
+void printBill() {
+    system("clear");
+    int count = 0;
+    bool close = false;
+    while (!close) {
+        system("clear");
+        int choice;
+        cout << "\t1.Add Bill." << endl;
+        cout << "\t2.Close Session." << endl;
+        cout << "\tEnter Choice: ";
+        cin >> choice;
 
-if(choice==1){
-string item;
-int quant;
-cout<<"\tEnter Item: ";
-cin>>item;
-cout<<"\tEnter Quantity: ";
-cin>>quant;
+        if (choice == 1) {
+            string item;
+            int quant;
+            cout << "\tEnter Item: ";
+            cin >> item;
+            cout << "\tEnter Quantity: ";
+            cin >> quant;
 
-ifstream in("D:/Bill.txt");
-ofstream out("D:/Bill Temp.txt");
+            ifstream in("Bill.txt");
+            ofstream out("Bill Temp.txt");
 
-string line;
-bool found = false;
+            string line;
+            bool found = false;
 
-while(getline(in, line)){
-stringstream ss;
-ss<<line;
-string itemName;
-int itemRate, itemQuant;
-char delimiter;
-ss>>itemName>>delimiter>>itemRate>>delimiter>>itemQuant;
+            while (getline(in, line)) {
+                stringstream ss;
+                ss << line;
+                string itemName;
+                int itemRate, itemQuant;
+                char delimiter;
+                ss >> itemName >> delimiter >> itemRate >> delimiter >> itemQuant;
 
-if(item ==itemName){
-found = true;
-if(quant <= itemQuant){
-int amount = itemRate * quant;
-cout<<"\t Item | Rate | Quantity | Amount"<<endl;
-cout<<"\t"<<itemName<<"\t "<<itemRate<<"\t "<<quant<<"\t "<<amount<<endl;
-int newQuant = itemQuant - quant;
-itemQuant = newQuant;
-count +=amount;
+                if (item == itemName) {
+                    found = true;
+                    if (quant <= itemQuant) {
+                        int amount = itemRate * quant;
+                        cout << "\t Item | Rate | Quantity | Amount" << endl;
+                        cout << "\t" << itemName << "\t " << itemRate << "\t " << quant << "\t " << amount << endl;
+                        int newQuant = itemQuant - quant;
+                        itemQuant = newQuant;
+                        count += amount;
 
-out<<"\t"<<itemName<<" : "<<itemRate<<" : "<<itemQuant<<endl;
-}
-else{
-cout<<"\tSorry, "<<item<<" Ended!"<<endl;
-}
-}
-else{
-out<<line<<endl;
-}	
-}
-if(!found){
-cout<<"\tItem Not Available!"<<endl;
-}
-out.close();
-in.close();
-remove("D:/Bill.txt");
-rename("D:/Bill Temp.txt", "D:/Bill.txt");
-}
-else if(choice ==2){
-close = true;
-cout<<"\tCounting Total Bill"<<endl;
-}
-Sleep(3000);
-}
-system("cls");
-cout<<endl<<endl;
-cout<<"\t Total Bill ----------------- : "<<count<<endl<<endl;
-cout<<"\tThanks For Shopping!"<<endl;
-Sleep(5000);
+                        out << "\t" << itemName << " : " << itemRate << " : " << itemQuant << endl;
+                    } else {
+                        cout << "\tSorry, " << item << " Ended!" << endl;
+                    }
+                } else {
+                    out << line << endl;
+                }
+            }
+            if (!found) {
+                cout << "\tItem Not Available!" << endl;
+            }
+            out.close();
+            in.close();
+            remove("Bill.txt");
+            rename("Bill Temp.txt", "Bill.txt");
+        } else if (choice == 2) {
+            close = true;
+            cout << "\tCounting Total Bill" << endl;
+        }
+        usleep(3000000); // Sleep for 3 seconds
+    }
+    system("clear");
+    cout << endl << endl;
+    cout << "\t Total Bill ----------------- : " << count << endl << endl;
+    cout << "\tThanks For Shopping!" << endl;
+    usleep(5000000); // Sleep for 5 seconds
 }
 
-int main(){
-Bill b;
+int main() {
+    Bill b;
 
-bool exit = false;
-while(!exit){
-system("cls");
-int val;
+    bool exit = false;
+    while (!exit) {
+        system("clear");
+        int val;
 
-cout<<"\tWelcome To Super Market Billing System"<<endl;
-cout<<"\t**************************************"<<endl;
-cout<<"\t\t1.Add Item."<<endl;
-cout<<"\t\t2.Print Bill."<<endl;
-cout<<"\t\t3.Exit."<<endl;
-cout<<"\t\tEnter Choice: ";
-cin>>val;
+        cout << "\tWelcome To Super Market Billing System" << endl;
+        cout << "\t**************************************" << endl;
+        cout << "\t\t1.Add Item." << endl;
+        cout << "\t\t2.Print Bill." << endl;
+        cout << "\t\t3.Exit." << endl;
+        cout << "\t\tEnter Choice: ";
+        cin >> val;
 
-if(val==1){
-system("cls");
-addItem(b);	
-Sleep(3000);
+        if (val == 1) {
+            system("clear");
+            addItem(b);
+            usleep(3000000); // Sleep for 3 seconds
+        } else if (val == 2) {
+            printBill();
+        } else if (val == 3) {
+            system("clear");
+            exit = true;
+            cout << "\tGood Luck!" << endl;
+            usleep(3000000); // Sleep for 3 seconds
+        }
+    }
 }
-
-else if(val==2){
-printBill();
-}
-
-else if(val==3){
-	system("cls");
-exit = true;
-cout<<"\tGood Luck!"<<endl;
-Sleep(3000);
-}	
-}
-}
+// g++ "/Users/tanmaisri/Downloads/Billing System.cpp" -o "/Users/tanmaisri/Downloads/Billing System" && "/Users/tanmaisri/Downloads/Billing System"
